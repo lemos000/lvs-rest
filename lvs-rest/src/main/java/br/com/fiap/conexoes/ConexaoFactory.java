5f176aaf5657c55d7bbef74dd8a0749fa8cda4cf
lemos000 <lemosgabriel2016@gmail.com>
2024-10-31 22:06:21 -0300

    :tada: primeiro commit da api

--- lvs-rest/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ lvs-rest/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -12,8 +12,7 @@ public class ConexaoFactory {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
 		return DriverManager.getConnection
-				("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl" ,
-						"rm554819" , "131004");
+				();
 	}
 
 }
